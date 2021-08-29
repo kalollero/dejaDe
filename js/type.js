59d2d1bf46a7628a99cbd287bbd8220a71f362c9
kalollero <kali.rso@hotmail.com>
2021-08-29 16:05:42 -0500

    Secciones 0.5

---
+++ js/type.js
@@ -0,0 +1,30 @@
+	tippy('#tippyhome', {
+		content: 'Inicio',
+		placement: 'top',
+ 	 	arrow: true,
+  	 	arrowType: 'round'
+	});
+
+	tippy('#tippyabout', {
+		content: 'Acerca del Libro',
+	});
+								
+	tippy('#tippydescription', {
+		content: 'Sinopsis',
+	});
+							
+	tippy('#tippyvideo', {
+		content: 'Video',
+	});
+								
+	tippy('#tippywriters', {
+		content: 'Autoras',
+	});
+								
+	tippy('#tippyreviews', {
+		content: 'Reseñas',
+	});
+
+	tippy('#tippystores', {
+		content: 'Que esperas',
+	});
